d67b0c531a1ff6626cde119abf1085d561f82877
Brendan Higgins <bjh83@case.edu>
2014-04-30 12:19:37 -0400

    I added the code for Vendors

--- Glamazon/app/models/admin/Database.scala
+++ Glamazon/app/models/admin/Database.scala
@@ -1,6 +1,7 @@
 package models.admin
 
 import scala.slick.driver.MySQLDriver.simple._
+import models.user.Products
 
 case class Administrator(id: Option[Int], userName: String, password: String, lastName: String, firstName: String, emailAddress: String)
 
@@ -13,4 +14,25 @@ class Administrators(tag: Tag) extends Table[Administrator](tag, "ADMINISTRATORS
   def emailAddress = column[String]("EMAIL_ADDRESS")
 
   def * = (id.?, userName, password, lastName, firstName, emailAddress) <> (Administrator.tupled, Administrator.unapply)
+}
+
+case class Vendor(id: Option[Int], name: String)
+
+class Vendors(tag: Tag) extends Table[Vendor](tag, "VENDORS") {
+  def id = column[Int]("VID", O.PrimaryKey, O.AutoInc)
+  def name = column[String]("Name")
+
+  def * = (id.?, name) <> (Vendor.tupled, Vendor.unapply)
+}
+
+case class VendingEntry(id: Option[Int], vendorId: Int, productId: Int)
+
+class Vending(tag: Tag) extends Table[VendingEntry](tag, "VENDING") {
+  def id = column[Int]("VENDINGID", O.PrimaryKey, O.AutoInc)
+  def vendorId = column[Int]("VID")
+  def productId = column[Int]("UPC")
+  def product = foreignKey("P_FK", productId, TableQuery[Products])(_.upc)
+  def vendor = foreignKey("V_FK", vendorId, TableQuery[Vendors])(_.id)
+
+  def * = (id.?, vendorId, productId) <> (VendingEntry.tupled, VendingEntry.unapply)
 }
